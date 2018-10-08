9865452e03de1be7e14e448471ba10c1933c527e
nguye <nguyenv2@outlook.com>
2018-10-08 10:55:53 -0700

    basic add, substract, multiply, divide done.

--- Prework-Calculator/Program.cs
+++ Prework-Calculator/Program.cs
@@ -6,7 +6,36 @@ namespace Prework_Calculator
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Add(2, 4);
+      Subtract(2, 4);
+      Multiply(2, 4);
+      Divide(2, 4);
+      Console.ReadKey();
+    }
+
+    public static float Add(float inputOne, float inputTwo)
+    {
+      float answer = inputOne + inputTwo;
+      Console.WriteLine($"{inputOne} + {inputTwo} is {answer}");
+      return answer;
+    }
+    public static float Subtract(float inputOne, float inputTwo)
+    {
+      float answer = inputOne - inputTwo;
+      Console.WriteLine($"{inputOne} - {inputTwo} is {answer}");
+      return answer;
+    }
+    public static float Multiply(float inputOne, float inputTwo)
+    {
+      float answer = inputOne * inputTwo;
+      Console.WriteLine($"{inputOne} * {inputTwo} is {answer}");
+      return answer;
+    }
+    public static float Divide(float inputOne, float inputTwo)
+    {
+      float answer = inputOne / inputTwo;
+      Console.WriteLine($"{inputOne} / {inputTwo} is {answer}");
+      return answer;
     }
   }
 }
