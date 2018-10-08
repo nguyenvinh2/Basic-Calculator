16dcf3cf35cfa3641a45a3984a01ada301881bca
nguye <nguyenv2@outlook.com>
2018-10-08 11:05:52 -0700

    read-me updates

--- Prework-Calculator/Program.cs
+++ Prework-Calculator/Program.cs
@@ -6,31 +6,37 @@ namespace Prework_Calculator
   {
     static void Main(string[] args)
     {
+      //Calls up all methods in calculator with specified parameters of 2 and 4
       Add(2, 4);
       Subtract(2, 4);
       Multiply(2, 4);
       Divide(2, 4);
+      //prevent console from automatically exiting
       Console.ReadKey();
     }
 
+    //summing method, writes to console and returns results.
     public static float Add(float inputOne, float inputTwo)
     {
       float answer = inputOne + inputTwo;
       Console.WriteLine($"{inputOne} + {inputTwo} is {answer}");
       return answer;
     }
+    //subtract method, writes to console and returns results.
     public static float Subtract(float inputOne, float inputTwo)
     {
       float answer = inputOne - inputTwo;
       Console.WriteLine($"{inputOne} - {inputTwo} is {answer}");
       return answer;
     }
+    //multiply method, writes to console and returns results.
     public static float Multiply(float inputOne, float inputTwo)
     {
       float answer = inputOne * inputTwo;
       Console.WriteLine($"{inputOne} * {inputTwo} is {answer}");
       return answer;
     }
+    //division method, writes to console and returns results.
     public static float Divide(float inputOne, float inputTwo)
     {
       float answer = inputOne / inputTwo;
--- READ ME.md
+++ READ ME.md
@@ -1,29 +1,29 @@
-# Basic 4-Function Calculator
+##Basic 4-Function Calculator
 
 This console applications takes in 2 parameters and adds, subtracts, multiplies, or divides depending on the method called.
 
-#Add
+##Add
 
 Returns the sum of the two parameters specified to the method.
 Writes the results to the console:
 
 e.g. "2 + 2 is 4"
 
-#Subtract
+##Subtract
 
 Returns the difference of the two parameters specified to the method.
 Writes the results to the console:
 
 e.g. "2 - 2 is 0"
 
-#Multiple
+##Multiple
 
 Returns the product of the two parameters specified to the method.
 Writes the results to the console:
 
 e.g. "2 * 2 is 4"
 
-#Subtract
+##Subtract
 
 Returns the division of the two parameters specified to the method.
 Writes the results to the console:
